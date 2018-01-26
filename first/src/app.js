5d16f64d20bb5e449ebc5a4df833022f62b27576
skywalker <skywalker_x@163.com>
2018-01-26 15:59:52 +0800

    context

--- first/src/app.js
+++ first/src/app.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {Component} from 'react';
+import PropTypes from 'prop-types'
 import ReactDom from 'react-dom';
 import Nav from 'nav/Nav.js';
 import CardWrap from 'cardWrap/CardWrap.js'
@@ -12,35 +13,54 @@ let dataArr = [
         name: 'Matt Giampietro',
         rel: '好友',
         desc: 'Matthew is an interior designer living in New York.',
-        year: '2013',
-        likeNum: '75'
+        year: 2013,
+        likeNum: 75
     },
     {
         imgSrc: require('image/molly.png'),
         name: 'Molly',
         rel: 'Coworker',
         desc: 'Molly is a personal assistant living in Paris.',
-        year: '2011',
-        likeNum: '105'
+        year: 2011,
+        likeNum: 105
     },
     {
         imgSrc: require('image/elyse.png'),
         name: 'Elyse',
         rel: 'Coworker',
         desc: 'Elyse is a copywriter working in New York.',
-        year: '2014',
-        likeNum: '75'
+        year: 2014,
+        likeNum: 75
     }
 ]
 
+//  使用context
+class App extends Component {
+    getChildContext(){
+        return {
+            et: 'Died'
+        }
+    };
+    render(){
+        let {data} = this.props;
+        return (
+            <div className={"ui container"}>
+                <div className={"ui dividing"}></div>
+                <Nav></Nav>
+                <CardWrap data={data}></CardWrap>
+            </div>
+        )
+    }
+}
+
+App.childContextTypes = {
+    et: PropTypes.string
+}
+
 
 ReactDom.render(
-      //JSX的语法
-    <div className={"ui container"}>
-        <div className={"ui dividing"}></div>
-        <Nav></Nav>
-        <CardWrap data={dataArr} ></CardWrap>
-    </div>,
+    //JSX的语法
+    <App data={dataArr}/>,
     document.getElementById('root')
 );
 
--- first/src/components/card/Card.js
+++ first/src/components/card/Card.js
@@ -1,8 +1,26 @@
 import React, {Component} from 'react'
+import PropTypes from 'prop-types'
+
+// 这里属性验证
+let propTypes = {
+    imgSrc: PropTypes.string,
+    name: PropTypes.string,
+    rel: PropTypes.string,
+    desc: PropTypes.string,
+    year: PropTypes.number,
+    likeNum: PropTypes.number
+}
+
+let contextTypes = {
+    et: PropTypes.string
+}
 
 export default class Card extends Component{
     render(){
         let { imgSrc, name, rel, desc, year, likeNum } = this.props;  // 这里结构赋值
+
+        let { et } = this.context;
+
         return (
             <div className={"ui card"}>
                 <div className={"image"}>
@@ -16,10 +34,13 @@ export default class Card extends Component{
                     <div className={"description"}>{desc}</div>
                 </div>
                 <div className={"extra content"}>
-                    <span className={"right floated"}>{`Joined in ${year}`}</span>
+                    <span className={"right floated"}>{`${et} in ${year}`}</span>
                     <span><i className={"empty heart icon"}></i>{` ${likeNum} like`}</span>
                 </div>
             </div>
         )
     }
 }
+
+Card.propTypes = propTypes;
+Card.contextTypes = contextTypes;
